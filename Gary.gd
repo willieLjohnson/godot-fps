bcb0a29503b5bd87db78fecd8c74d8ef0818b992
root <root@DESKTOP-GE48KEJ.localdomain>
2019-05-07 17:51:34 -0400

    Add player movement with gravity

--- Gary.gd
+++ Gary.gd
@@ -6,14 +6,83 @@ var mouse_sensitivity = 0.3
 var velocity = Vector3()
 var direction = Vector3()
 
+# Fly variables
 const FLY_SPEED = 40
 const FLY_ACCEL = 4
+
+# Walk variables
+var gravity = -9.8 * 3
+const MAX_SPEED = 20
+const MAX_RUNNING_SPEED = 30
+const ACCEL = 2
+const DEACCEL = 6
+
+# Jumping
+var jump_height = 15
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	walk(delta)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		
+		var change = -event.relative.y * mouse_sensitivity
+		if change + camera_angle < 90 and change + camera_angle > -90:
+			$Head/Camera.rotate_x(deg2rad(change))
+			camera_angle += change
+
+func walk(delta):
+	# Set the direction of the player
+	direction = Vector3()
+
+	# Get the rotation of the camera
+	var aim = $Head/Camera.get_global_transform().basis
+	if Input.is_action_pressed("move_forward"):
+		direction -= aim.z
+	if Input.is_action_pressed("move_backward"):
+		direction += aim.z
+	if Input.is_action_pressed("move_left"):
+		direction -= aim.x
+	if Input.is_action_pressed("move_right"):
+		direction += aim.x
+
+	direction = direction.normalized()
+
+	velocity.y += gravity * delta
+
+	var temp_velocity = velocity
+	temp_velocity.y = 0
+
+	var speed
+	if Input.is_action_pressed("move_sprint"):
+		speed = MAX_RUNNING_SPEED
+	else:
+		speed = MAX_SPEED
+
+	# where would the play go at max speed
+	var target = direction * speed
+	
+	var acceleration
+	if direction.dot(temp_velocity) > 0:
+		acceleration = ACCEL
+	else:
+		acceleration = DEACCEL
+
+	# Calculate a portion of the distance to go
+	temp_velocity = velocity.linear_interpolate(target, acceleration * delta)
+
+	velocity.x = temp_velocity.x
+	velocity.z = temp_velocity.z
+
+	# move
+	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
+
+func fly(delta):
 	# Set the direction of the player
 	direction = Vector3()
 
@@ -38,12 +107,3 @@ func _physics_process(delta):
 
 	# move
 	move_and_slide(velocity)
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		$Head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
-		
-		var change = -event.relative.y * mouse_sensitivity
-		if change + camera_angle < 90 and change + camera_angle > -90:
-			$Head/Camera.rotate_x(deg2rad(change))
-			camera_angle += change
